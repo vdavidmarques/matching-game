1b43c2d074592ac2fd890f783ed84f2a89344150
vdavidmarques <vdavidmarques@gmail.com>
2025-11-17 19:51:32 -0300

    feat: Add PlayerTitle component

---
+++ src/components/PlayerTitle/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+function PlayerTitle(number){
+    return `
+        <h2 class="title">Player <span>${number}</span></h2>
+    `
+}
+
+export default PlayerTitle
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+import "./styles/settings/colors.css";
+import "./styles/generic/reset.css";
+import "./styles/elements/base.css";
+
 import CardGame from "./components/CardGame";
+import PlayerTitle from "./components/PlayerTitle";
 
 const $root = document.querySelector("#root");
-const $htmlCardGame = CardGame();
 
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+//const $htmlCardGame = "<section class='cards'>" + CardGame().repeat(6) + "</section>";
+const $htmlPlayerTitle = "<section class='player-title'>" + PlayerTitle(1) + PlayerTitle(2) + "</section>";
+
+$root.insertAdjacentHTML("afterbegin", $htmlPlayerTitle);
+$root.insertAdjacentHTML("beforeend", CardGame());
